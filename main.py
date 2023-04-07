dea3c2c508025dab71365215fc20aa688f80ec3d
rafi-sama <rafimaliki28@gmail.com>
2023-04-07 10:59:55 +0700

    new file:   __pycache__/commands.cpython-39.pyc 	new file:   __pycache__/data.cpython-39.pyc 	new file:   __pycache__/datas.cpython-39.pyc 	renamed:    fungsi.py -> commands.py 	new file:   file/bahan_bangunan.csv 	new file:   file/candi.csv 	new file:   file/user.csv 	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-import fungsi
+# File: main.py
+import commands
+
+# Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
+users = [] # Matriks data user
+candi = [] # Matriks data candi
+bahan_bangunan = [] # Data bahan bangunan
+
+
+# Kodingan chat gpt
+
+# gak ngerti gw cooo ini jg gk work sih codenya
+
+import argparse
+from data import load
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Load a CSV file into a variable.')
+    parser.add_argument('file_name', type=str, help='the name of the CSV file to load')
+    parser.add_argument('variable', type=str, help='the name of the variable to assign the CSV data to')
+    args = parser.parse_args()
+
+    load(args.file_name, args.variable)
+
+load("file/user.csv", users)
+load("file/candi.csv", candi)
+load("file/bahan_bangunan.csv", bahan_bangunan)
+
+# while True:
+#   masukan = input(">>> ")
+#   commands.run(masukan)
+
+print(users)
