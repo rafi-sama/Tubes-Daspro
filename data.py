ce3951d6a2f142787b6ed507e7adcc31f158d953
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-08 16:45:06 +0700

    finishing user data processing

--- data.py
+++ data.py
@@ -1,11 +1,7 @@
-# Kodingan Chat GPT
-
 import csv
 
 def load(file_name, variable):
     with open(file_name, 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
-            
-load(r"C:\Users\Qk\Documents\College\02. Programming Fundamentals\Assignment\Python\Big Project\Tubes-Daspro\file\candi.csv", "pembuat")
+        reader = csv.reader(file, delimiter = ";")
+        variable = [row for row in reader]
+    return variable
