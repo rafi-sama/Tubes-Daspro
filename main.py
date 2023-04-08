f9662b2c3c64e09c85bcc83f6a2a3ef43a610b2d
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-08 16:45:50 +0700

    finishing main program

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 # File: main.py
 import commands
+import argparse
+from data import load
 
 # Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
 users = [] # Matriks data user
 candi = [] # Matriks data candi
 bahan_bangunan = [] # Data bahan bangunan
 
+#Mengisi users, candi, dan bahan_bangunan menggunakan file
+users = load(r"file\user.csv", users) # Matrix data user
+candi = load(r"file\candi.csv", candi) # Matrix data user
+bahan_bangunan = load(r"file\bahan_bangunan.csv", bahan_bangunan) # Matrix data user
 
-# Kodingan chat gpt
+role = [0] # Untuk menentukan apakah ada user yang sedang login atau tidak
 
-# gak ngerti gw cooo ini jg gk work sih codenya
-
-import argparse
-from data import load
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Load a CSV file into a variable.')
-    parser.add_argument(r'C:\Users\Qk\Documents\College\02. Programming Fundamentals\Assignment\Python\Big Project\Tubes-Daspro\file\candi.csv', type=str, help='the name of the CSV file to load')
-    parser.add_argument('pembuat', type=str, help='the name of the variable to assign the CSV data to')
-    args = parser.parse_args()
-
-    load(args.file_name, args.variable)
-
-load("file/user.csv", users)
-load("file/candi.csv", candi)
-load("file/bahan_bangunan.csv", bahan_bangunan)
-
-# while True:
-#   masukan = input(">>> ")
-#   commands.run(masukan)
-
-print(users)
+# Menerima masukan
+while True:
+    masukan = input(">>> ")
+    commands.run(masukan,users,candi,bahan_bangunan,role)
