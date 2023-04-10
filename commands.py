a6dd77d7f63aae5ef7703ba545bcffdd7059059d
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 21:56:47 +0700

    command help, command listuser

--- commands.py
+++ commands.py
@@ -2,6 +2,13 @@ from functions import *
 
 def run(inputan, users, candi, bahan_bangunan, role):
 
+    # fungi sementara buat liat list user (ntar dihapus)
+    if inputan == "listuser":
+        N = len_count(users)
+
+        for i in range(1,N):
+            print(users[i])
+
     if inputan == "help":
         help(role)
 
@@ -146,19 +153,75 @@ def summonjin(users):
 def help(role):
     
     if role[1] == 0:
-        print("Menu help belum login")
+        print('''=========== HELP ===========
+1. login
+   Untuk masuk menggunakan akun
+2. load
+   Untuk memuat file eksternal ke dalam permainan
+3. exit
+   Untuk keluar dari permainan
+4. help
+   Untuk menampilkan command yang dapat dipanggil
+============================''')
     
     elif role[1] == "bandung_bondowoso":
-        print("Menu help bandung bondowoso")
+        print('''=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. summonjin
+   Untuk memanggil jin dari dunia lain
+3. hapusjin
+   Untuk menghapus jin yang telah dipanggil
+4. ubahjin
+   Untuk mengubah tipe jin yang telah dipanggil
+5. batchkumpul
+   Untuk mengerahkan seluruh pasukan jin pengumpul
+6. batchbangun
+   Untuk mengerahkan seluruh pasukan jin pengumpul
+7. laporanjin
+   Untuk mengambil laporan kinerja jin
+8. laporancandi
+   Untuk mengambil laporan progress pembangunan candi
+9. exit
+   Untuk keluar dari permainan
+10. help
+    Untuk menampilkan command yang dapat dipanggil
+============================''')
 
     elif role[1] == "roro_jonggrang":
-        print("Menu help roro jonggrang")
+        print('''=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. hancurkancandi
+   Untuk menghancurkan candi
+3. ayamberkokok
+   Untuk memalsukan ayam berkokok di pagi hari
+4. exit
+   Untuk keluar dari permainan
+5. help
+   Untuk menampilkan command yang dapat dipanggil
+============================''')
 
     elif role[1] == "Pengumpul":
-        print("Menu help jin pengumpul")
+        print('''=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. kumpul
+   Untuk mengumpulkan bahan-bahan yang diperlukan untuk membangun candi
+3. exit
+   Untuk keluar dari permainan
+4. help
+   Untuk menampilkan command yang dapat dipanggil
+============================''')
 
     else: # role[1] == "Pembangun"
-        print("Menu help jin pembangun")
-
-
-
+        print('''=========== HELP ===========
+1. logout
+   Untuk keluar dari akun yang digunakan sekarang
+2. bangun
+   Untuk membangun candi
+3. exit
+   Untuk keluar dari permainan
+4. help
+   Untuk menampilkan command yang dapat dipanggil
+============================''')
