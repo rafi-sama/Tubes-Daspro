53479f5ca09b32c35d996aa89a4e42fbdb48fd83
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 14:16:24 +0700

    new file:   testing.py 	modified:   __pycache__/functions.cpython-311.pyc 	modified:   functions.py

---
+++ testing.py
@@ -0,0 +1,30 @@
+from functions import *
+
+user_csv = open("file/user.csv",'r')
+
+user_csv = [row for row in user_csv]
+
+print(user_csv)
+
+# for row in user_csv:
+#     print(row)
+
+baris = len_count(user_csv)
+
+kolom = 0
+temp = user_csv[0]
+
+for i in range(len_count(temp)):
+    if temp[i] == ";":
+        kolom = 0
+
+kolom += 1
+
+user = ["" for i in range(baris)]
+
+
+for i in range(baris):
+
+    user[i] = split(user_csv[i], ";")
+
+print(user)
