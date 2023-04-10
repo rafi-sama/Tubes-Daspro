02e985dcbcbcf3c07d6f06ca738ac4620b74deb9
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 21:15:14 +0700

    fix load tanpa import csv

--- data.py
+++ data.py
@@ -1,7 +1,21 @@
-import csv
+from functions import *
 
 def load(file_name, variable):
+
     with open(file_name, 'r') as file:
-        reader = csv.reader(file, delimiter = ";")
-        variable = [row for row in reader]
+
+        # mengubah file menjadi bentuk array/list
+        reader = [row for row in file]
+
+        baris = len_count(reader)
+
+        # membuat array kosong sebanyak baris
+        tempArr = ["" for i in range(baris)]
+
+        # looping untuk mengisi setiap baris dengan array string yang sudah di split
+        for i in range(baris):
+            tempArr[i] = split(reader[i], ";")
+
+        variable = tempArr
+
     return variable
