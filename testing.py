0c5c46b2fe9b18254fc2a26e582ca2a8e0608732
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 21:16:15 +0700

    komen all

--- testing.py
+++ testing.py
@@ -1,30 +1,30 @@
-from functions import *
+# from functions import *
 
-user_csv = open("file/user.csv",'r')
+# user_csv = open("file/user.csv",'r')
 
-user_csv = [row for row in user_csv]
+# user_csv = [row for row in user_csv]
 
-print(user_csv)
+# print(user_csv)
 
-# for row in user_csv:
-#     print(row)
+# # for row in user_csv:
+# #     print(row)
 
-baris = len_count(user_csv)
+# baris = len_count(user_csv)
 
-kolom = 0
-temp = user_csv[0]
+# kolom = 0
+# temp = user_csv[0]
 
-for i in range(len_count(temp)):
-    if temp[i] == ";":
-        kolom = 0
+# for i in range(len_count(temp)):
+#     if temp[i] == ";":
+#         kolom = 0
 
-kolom += 1
+# kolom += 1
 
-user = ["" for i in range(baris)]
+# user = ["" for i in range(baris)]
 
 
-for i in range(baris):
+# for i in range(baris):
 
-    user[i] = split(user_csv[i], ";")
+#     user[i] = split(user_csv[i], ";")
 
-print(user)
+# print(user)
