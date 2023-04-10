491593ca9a2e1833e31079f1888f3aa2a4c08155
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 21:14:48 +0700

    nambahin komen summonjin

--- commands.py
+++ commands.py
@@ -69,8 +69,17 @@ def logout(role):
 
 def summonjin(users):
 
+    # variabel 'users' berisi data:
+
+    # kolom ke-
+    # 0 : username | 1 : password | 2 : role
+    # baris ke-
+    # 0 : judul kolom | 1 : data bondowoso | 2 : data roro | 3 dst : data jin
+
     N = len_count(users)
 
+    # membuat array yang berisi nama jin
+    # in range(3, N-2) karena data jin pertama kali ada pada baris ke 3
     nama_jin = [users[i][0] for i in range(3, N-2)]
     
     jenis_jin = ["Pengumpul", "Pembangun"] # dengan nomor 0, 1
@@ -81,10 +90,11 @@ def summonjin(users):
     print("  (2) Pembangun - Bertugas membangun candi")
 
 
-    # Input nomor jin
+    # INPUT NOMOR JIN
 
     nomor_jin_input = input("\nMasukkan nomor jenis jin yang ingin dipanggil: ") 
 
+    # validasi agar inputan nomor jin sesuai
     while (nomor_jin_input not in nomor_jin):
         
         print(f"\nTidak ada jenis jin bernomor \"{nomor_jin_input}\"!")
@@ -93,22 +103,24 @@ def summonjin(users):
     print(f"\nMemilih jin \"{jenis_jin[int(nomor_jin_input)-1]}\"")
 
 
-    # Input nama jin
+    # INPUT NAMA JIN
 
     nama_jin_input = input("\nMasukkan username jin: ")
     
+    # validasi agar nama jin tidak ada yang sama
     while (nama_jin_input in nama_jin):
         
         print(f"Username \"{nama_jin_input}\" sudah diambil!")
         nama_jin_input = input("\nMasukkan username jin: ")
 
 
-    # Input password jin
+    # INPUT PASSWORD JIN
 
     password_jin_input = input("Masukkan password jin: ")
 
     N = len_count(password_jin_input)
     
+    # validasi agar password sesuai ketentuan
     while (N < 5 or N > 25):
         
         print("\nPassword panjangnya harus 5-25 karakter!")
@@ -116,6 +128,7 @@ def summonjin(users):
         password_jin_input = input("Masukkan password jin: ")
 
         N = len_count(password_jin_input)
+        
         
     # Mengappend data baru jin ke users (tanpa .append())
 
