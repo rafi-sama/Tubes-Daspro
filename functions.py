70c0597b8e75ccc4fe25c2f2716f3167a34ff6ef
rafi-sama <rafimaliki28@gmail.com>
2023-04-10 14:22:18 +0700

    modified:   functions.py

--- functions.py
+++ functions.py
@@ -6,3 +6,36 @@ def len_count(x):
         N += 1
     
     return N
+
+def split(data, separator):
+
+    NSeparator = 0
+
+    len = len_count(data)
+    
+    for i in range(len):
+        if data[i] == separator:
+            NSeparator += 1
+        
+    tempArr = ["" for i in range(NSeparator + 1)]
+    tempElement = ""
+    kolom = 0
+
+    for i in range(len):
+
+        if data[i] == separator:
+            
+            tempArr[kolom] = tempElement
+            tempElement = ""
+            kolom += 1
+
+        elif i == len-1 :
+        
+            tempArr[kolom] = tempElement
+
+            break
+        else:
+
+            tempElement += data[i]
+        
+    return tempArr
