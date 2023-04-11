0831083bc590275dab284fed5a5fb6b7c13b480b
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-11 09:10:22 +0700

    merombak fungsi utility

--- functions.py
+++ functions.py
@@ -1,28 +1,90 @@
-def len_count(x):
+# fungsi yang menambahkan arr + element (element di indeks terakhir)
+def konso(arr, element):
 
-    N = 0
+    len = arr_len(arr)
 
-    # untuk setiap elemen pada X, nilai N bertambah 1
-    for i in x:
-        N += 1
+    temp = ["" for i in range(len+1)]
+
+    for i in range(len):
+        temp[i] = arr[i]
     
-    return N
+    temp[len] = element
 
-def split(data, separator):
+    return temp
+
+# fungsi yang menambahkan element + arr (element di indeks pertama)
+def konsDot(element, arr):
+
+    len = arr_len(arr)
+
+    temp = ["" for i in range(len+1)]
+
+    for i in range(1, len+1):
+        temp[i] = arr[i]
+    
+    temp[0] = element
+
+    return temp  
+
+# fungsi yang menggabungkan 2 arr
+def konkat(arr1, arr2):
+    
+    len1 = arr_len(arr1) 
+    len2 = arr_len(arr2)
+
+    temp = ["" for i in range(len1+len2)]
+
+    for i in range(len1):
+        temp[i] = arr1[i]
+
+    for j in range(len1, len1+len2):
+        temp[j] = arr2[j-len1]
+        print(temp)
+
+    return temp
+
+# fungsi yang menentukan panjang string
+def str_len(string):
+
+    len = 0
+    temp = ""
+
+    while temp != string:
+
+        temp += string[len]
+        len += 1
+    
+    return len
+
+# fungsi yang menentukan panjang array
+def arr_len(arr):
+
+    len = 0
+    temp = []
+
+    while temp != arr:
+        
+        temp = konso(temp, arr[len])
+        len += 1
+
+    return len
+
+
+def split(string, delimiter):
 
     # variabel yang menyatakan banyaknya pemisah (ex: titik koma pada file .csv)
-    NSeparator = 0 
+    NDelimiter = 0 
 
     # banyaknya karakter pada string yang ingin di split
-    len = len_count(data) 
+    len = str_len(string) 
 
     # menghitung banyaknya separator
     for i in range(len): 
-        if data[i] == separator:
-            NSeparator += 1
+        if string[i] == delimiter:
+            NDelimiter += 1
     
     # array yang akan diisi dengan elemen string yang sudah di split
-    tempArr = ["" for i in range(NSeparator + 1)]
+    tempArr = ["" for i in range(NDelimiter + 1)]
 
     # elemen pada array yang akan di split
     tempElement = ""
@@ -34,7 +96,7 @@ def split(data, separator):
     for i in range(len):
 
         # jika pada looping ketemu separator
-        if data[i] == separator:
+        if string[i] == delimiter:
             
             # tempElement di assign ke current kolom
             tempArr[kolom] = tempElement
@@ -56,7 +118,7 @@ def split(data, separator):
         else:
 
             # karakter akan di tambahkan ke tempElement
-            tempElement += data[i]
+            tempElement += string[i]
     
     # mengeembalikan array yang berisi string yang sudah di split
     return tempArr
