f079d4a820c0016ac6fb5ec34f6a03888132acae
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-13 01:06:04 +0700

    making save system foundation

--- function/f14_save.py
+++ function/f14_save.py
@@ -1,2 +1,4 @@
 def save():
-    print("Ini buat save")
+    with open(r"file\user.csv", 'a') as file_users:
+        users = file_users.write('\n' + new_user)
+        print("Data successfully added!")
