c7b09873eea9c8a43254c693f3cd1d7783361187
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-13 21:08:48 +0700

    finishing exit function

--- function/f16_exit.py
+++ function/f16_exit.py
@@ -1,11 +1,11 @@
-def exit():
+import function.f14_save as f14
+def exit(users, candi, bahan_bangunan):
     choice = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)\n")
     while True:
-        if choice == "y":
-            save()
-            print("Save sukses, sampai jumpa!")
+        if choice == "y" or choice == "Y":
+            f14.save(users, candi, bahan_bangunan)
             break
-        elif choice == "n":
+        elif choice == "n" or choice == "N":
             print("Keluar dari game tanpa melakukan save.")
             break
         else:
