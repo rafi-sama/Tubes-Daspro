878b2dde78815e5c23cb8301668bdf762f44479a
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-13 21:08:31 +0700

    finishing ubahjin

--- function/f05_ubahjin.py
+++ function/f05_ubahjin.py
@@ -1,8 +1,8 @@
 from functions import *
 def ubahjin(list_jin) :
     username_jin = input("Masukkan username jin: ")
-    found=False
-    N=arr_len(list_jin)
+    found = False
+    N = arr_len(list_jin)
 
     for i in range (N) :
         if username_jin == list_jin[i][0] :
@@ -16,12 +16,13 @@ def ubahjin(list_jin) :
             determine = input("Jin ini bertipe “Pengumpul”. Yakin ingin mengubah ke tipe “Pembangun” (Y/N)? ")
             while True :
                 if determine == "Y" or determine == "y" :
-                    list_jin[lokasi][2] == "Pembangun"
-                    list_jin[:]=list_jin
+                    list_jin[lokasi][2] = "Pembangun"
+                    list_jin[:] = list_jin
                     print("Jin telah berhasil diubah.")
                     break
                 elif determine == "N" or determine == "n" :
                     print("Jin tidak jadi diubah.")
+                    list_jin[:] = list_jin
                     break
                 else :
                     print(f"Hanya menerima input y/n. Silahkan ulangi.")
@@ -30,12 +31,13 @@ def ubahjin(list_jin) :
             determine = input("Jin ini bertipe “Pembangun”. Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)? ")
             while True :
                 if determine == "Y" or determine == "y" :
-                    list_jin[lokasi][2] == "Pengumpul"
-                    list_jin[:]=list_jin
+                    list_jin[lokasi][2] = "Pengumpul"
+                    list_jin[:] = list_jin
                     print("Jin telah berhasil diubah.")
                     break
                 elif determine == "N" or determine == "n" :
                     print("Jin tidak jadi diubah.")
+                    list_jin[:] = list_jin
                     break
                 else :
                     print(f"Hanya menerima input y/n. Silahkan ulangi.")
