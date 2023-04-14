a7048cdf082edd41910ab8b674d0751438eac8a6
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-04-14 20:04:22 +0700

    finishing ayamberkokok function

---
+++ function/f12_ayamberkokok.py
@@ -0,0 +1,14 @@
+from functions import *
+
+def ayamberkokok(candi):
+    N = arr_len(candi)-1
+    if N < 100:
+        print("Kukuruyuk.. Kukuruyuk.. \n")
+        print(f"Jumlah candi: {N} \n")
+        print("Selamat, Roro Jonggrang memenangkan permainan! \n")
+        print("*Bandung Bondowoso angry noise*")
+        print("Roro Jonggrang dikutuk menjadi candi.")
+    else:
+        print("Kukuruyuk.. Kukuruyuk.. \n")
+        print(f"Jumlah candi: {N} \n")
+        print("Yah, Bandung Bondowoso memenangkan permainan!")
