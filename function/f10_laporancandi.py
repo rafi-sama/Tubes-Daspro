8727909c9d94f3355f7c6aeaf27e2b14494824c6
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2023-04-16 21:01:29 +0700

    jj

--- function/f10_laporancandi.py
+++ function/f10_laporancandi.py
@@ -1,80 +1,94 @@
 from functions import *
 
+total_pasir = 0
+total_batu = 0                    #semua bahan-bahan dibikin 0 dulu
+total_air = 0
+arr_pasir = []  
+arr_batu = []
+arr_air = []
+harga_candi = []
 
-
-
-
+#TOTAL CANDI
 def LaporanCandi (candi):
-    #TOTAL CANDI
-    sum_candi = arr_len(candi) - 1
-    total_pasir = 0
-    total_batu = 0                    #semua bahan-bahan dibikin 0 dulu
-    total_air = 0
-    arr_pasir = []  
-    arr_batu = []
-    arr_air = []
-    harga_candi = []                       #variable ini berisi list total harga candi dari perhitungan
-    #TOTAL PASIR, AIR,DAN BATU YANG DIGUNAKAN
-  
+
+    #Globalisasi Varb
+
+    global total_pasir #variable ini berisi total bahan dari pasir
+    global total_batu #variable ini berisi total bahan dari batu
+    global total_air #variable ini berii total bahan dari air
+    global arr_air #variable ini berisi list bahan air
+    global arr_batu #variable ini berisi list bahan dari batu
+    global arr_pasir #variable ini berisi list bahan dari pasir
+    global harga_candi #variable ini berisi list total harga candi dari perhitungan
+    
+
+#TOTAL PASIR, AIR,DAN BATU YANG DIGUNAKAN
+
     for i in range (1,arr_len(candi)):    #iterasi dari baris kedua ke baris terakhir
         total_pasir += int(candi[i][2])
         total_batu += int(candi[i][3])       #penambahan tiap data pada masing-masing bahan
         total_air += int(candi[i][4])        #ke dalam variable yang sudah dikosongkan
         i += 1
-    
-    
-    #ID CANDI TERMAHAL & TERKECIL
-    #1: Deteksi Bahan                                    
+
+
+#ID CANDI TERMAHAL
+#1: Deteksi Bahan                                    
     for i in range (1,arr_len(candi)):    #masukkan semua data bahan dalam variable list
         arr_pasir += (candi[i][2])
         arr_batu += (candi[i][3])         #proses yang dilakukan sama, namun variable yang dihasilkan
         arr_air += (candi[i][4])          #bertipe nested list seperti pasir = [2,4,3,1], air = [3,5,6,1]
         i += 1
-    
-   
-   #2: perhitungan
+
+
+#2: perhitungan
     c = 0
+
     for i in range(arr_len(arr_air)):
         total_i = 10000*(int(arr_pasir[c])) + 15000*(int(arr_batu[c])) + 7500*(int(arr_air[c])) 
         harga_candi += [total_i]  
         c += 1
-        
     
-    #3: Penentuan Candi Termahal dan Terkecil
+#PENENTUAN CANDI TERBESAR DAN TERKECIL
     maks = harga_candi[0]   
     min = harga_candi[0]
-    for c in harga_candi:               #karena list harga_candi yang berisi harga-harga pada candi yang dibangun sudah dibuat,        
-        if c > maks:                    #kita tinggal menentukan data terbesar atau terkecil dari list tersebut
+    for c in harga_candi:               #karena list harga_candi sudah dibuat, kita tinggal menentukan data terbesar         
+        if c > maks:                    #atau terkecil dari list tersebut
             maks = c
         elif c < min:
             min = c    
 
+    max_score = -1
+    max_id = ''
+    for i in range(1, len(candi)):
+        cur_id = candi[i][0]
+        cur_pasir = int(candi[i][2])
+        cur_batu = int(candi[i][3])
+        cur_air = int(candi[i][4])
+
+        cur_score = cur_pasir + cur_batu + cur_air
+
+        if cur_score > max_score:
+            max_score = cur_score
+            max_id = cur_id
     
     
-    if sum_candi == 0:
-        total_pasir = 0
-        total_batu = 0                    #sebenarnya syarat ini tidak perlu, tapi saya takut akan adanya 
-        total_air = 0                     #kesalahan perhitungan
-        candi_mahal = '-'
-        candi_murah = '-'
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 
-        
-    
-    
-    
-    
-    
-    
-    
-    
     
 
+
+    print(f"Total Candi: {arr_len(candi)-1} \n")
     print(f"Total Pasir yang digunakan: {total_pasir} \n")
     print(f"Total Batu yang digunakan: {total_batu} \n")
     print(f"Total Air yang digunakan: {total_air} \n")
-    print(f"ID Candi Termahal: {candi_mahal} \n")
-    print(f"ID Candi Termurah: {candi_murah} \n")
-
-
+    print(f"ID Candi Termahal: {max_id} (Rp: {maks}) \n")
+    print(f"ID Candi Termurah: (Rp: {min}) \n")
--- testing.py
+++ testing.py
@@ -29,62 +29,7 @@
 
 # print(user)
 
-from functions import *
-x = [["id","pembuat","pasir","batu","air"],
-     ["1","kirito","2","3","4"],
-     ['2','asuna','4','1','3'],
-     ['3','rimuru','3','1','3'],
-     ['4','tempest','1','2','4'],
-     ['5','ud','5','6','3']]
 
-
-
-#TOTAL CANDI
-sum_candi = arr_len(x) - 1
-total_pasir = 0
-total_batu = 0                    #semua bahan-bahan dibikin 0 dulu
-total_air = 0
-arr_pasir = []  
-arr_batu = []
-arr_air = []
-harga_candi = []                       #variable ini berisi list total harga x dari perhitungan
-#TOTAL PASIR, AIR,DAN BATU YANG DIGUNAKAN
-
-for i in range (1,arr_len(x)):    #iterasi dari baris kedua ke baris terakhir
-    total_pasir += int(x[i][2])
-    total_batu += int(x[i][3])       #penambahan tiap data pada masing-masing bahan
-    total_air += int(x[i][4])        #ke dalam variable yang sudah dikosongkan
-    i += 1
-
-
-#ID CANDI TERMAHAL
-#1: Deteksi Bahan                                    
-for i in range (1,arr_len(x)):    #masukkan semua data bahan dalam variable list
-    arr_pasir += (x[i][2])
-    arr_batu += (x[i][3])         #proses yang dilakukan sama, namun variable yang dihasilkan
-    arr_air += (x[i][4])          #bertipe nested list seperti pasir = [2,4,3,1], air = [3,5,6,1]
-    i += 1
-
-
-#2: perhitungan
-c = 0
-
-for i in range(arr_len(arr_air)):
-    total_i = 10000*(int(arr_pasir[c])) + 15000*(int(arr_batu[c])) + 7500*(int(arr_air[c])) 
-    harga_candi += [total_i]  
-    c += 1
-    
-#PENENTUAN CANDI TERBESAR DAN TERKECIL
-maks = harga_candi[0]   
-min = harga_candi[0]
-for c in harga_candi:               #karena list harga_candi sudah dibuat, kita tinggal menentukan data terbesar         
-    if c > maks:                    #atau terkecil dari list tersebut
-        maks = c
-    elif c < min:
-        min = c    
-num_min =  0                       #varb ini berisi urutan/nomor dari candi dengan bahan termurah dan termahal
-num_maks = 0
-    
     
     
     
@@ -99,20 +44,6 @@ num_maks = 0
 
     
 
-if sum_candi == 0:
-    total_pasir = 0
-    total_batu = 0                    #sebenarnya syarat ini tidak perlu, tapi saya takut akan adanya 
-    total_air = 0                     #kesalahan perhitungan
-
-
-candi_mahal = 0
-candi_murah = 0
-print(f"Total Candi: {sum_candi} \n")
-print(f"Total Pasir yang digunakan: {total_pasir} \n")
-print(f"Total Batu yang digunakan: {total_batu} \n")
-print(f"Total Air yang digunakan: {total_air} \n")
-print(f"ID Candi Termahal: {min} Rp: ({maks}) \n")
-print(f"ID Candi Termurah: Rp: ({min}) \n")
 
 
 
