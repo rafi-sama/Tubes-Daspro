b336676c03d0ebcd397537b8250de8e142dc4b64
rafi-sama <rafimaliki28@gmail.com>
2023-04-18 11:02:56 +0700

    rapihin load

--- commands/f13_load.py
+++ commands/f13_load.py
@@ -1,21 +1,7 @@
 from functions import *
 
-def load(file_name, variable):
+def load(file_name):
 
     with open(file_name, 'r') as file:
 
-        # mengubah file menjadi bentuk array/list
-        reader = [row for row in file]
-
-        baris = arr_len(reader)
-
-        # membuat array kosong sebanyak baris
-        tempArr = ["" for i in range(baris)]
-
-        # looping untuk mengisi setiap baris dengan array string yang sudah di split
-        for i in range(baris):
-            tempArr[i] = split(reader[i], ";")
-
-        variable = tempArr
-
-    return variable
+        return convertToArr(file, ";")
