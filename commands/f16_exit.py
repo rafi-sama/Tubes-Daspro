f1093be7cc524369540ae9790c50dcff6edcca31
rizqikapratamaa <rizqikapratamaa@gmail.com>
2023-05-01 22:20:55 +0700

    update

--- commands/f16_exit.py
+++ commands/f16_exit.py
@@ -7,9 +7,11 @@ def exit(users, candi, bahan_bangunan, running, trash):
         if choice == "y" or choice == "Y":  # Jika input dari user adalah "Y" atau "y"
             f14.save(users, candi, bahan_bangunan, trash)  # Lakukan save
             running[0] = 0  # Ubah running ke 0 untuk menghentikan program
+            break
         elif choice == "n" or choice == "N":    # Jika input dari user adalah "Y" atau "y"
             print("Keluar dari game tanpa melakukan save.")
             running[0] = 0  # Ubah running ke 0 untuk menghentikan program
+            break
         else:   # Jika input selain di atas, ulangi sampai user memasukkan input yang sesuai
             print("Input (y/n) untuk melakukan save atau tidak.")
             choice = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n)\n")
